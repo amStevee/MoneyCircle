e861aee1905f2da43f5cfda8826808993ee78853
amStevee <stevenanongo2@gmail.com>
2026-08-14 13:15:08 -0700

    Refactor code structure for improved readability and maintainability

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,33 +1,17 @@
-
-
-const LINKS = [
-  {
-    title: "Docs",
-    href: "https://turborepo.dev/docs",
-    description: "Find in-depth information about Turborepo features and API.",
-  },
-  {
-    title: "Learn",
-    href: "https://turborepo.dev/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
-  },
-  {
-    title: "Templates",
-    href: "https://turborepo.dev/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
-  },
-  {
-    title: "Deploy",
-    href: "https://vercel.com/new",
-    description:
-      "Instantly deploy your Turborepo to a shareable URL with Vercel.",
-  },
-];
+import { Faq1 } from "@/components/faq1"
+import { Hero47 } from "@/components/hero47"
+import { Navbar1 } from "@/components/navbar1"
+import { Process1 } from "@/components/process1"
+import { Waitlist1 } from "@/components/waitlist1"
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <h1>Landing Page</h1>
-    </main>
-  );
+    <div className="p-5">
+      <Navbar1 />
+      <Hero47 />
+      <Process1 />
+      <Faq1 />
+      <Waitlist1 />
+    </div>
+  )
 }

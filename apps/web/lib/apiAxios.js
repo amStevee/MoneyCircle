288316bea6f2e18f4c38652e99cb9f4bccd736bc
amStevee <stevenanongo2@gmail.com>
2026-08-20 14:15:25 -0700

    add vercel url to cors

--- apps/web/lib/apiAxios.js
+++ apps/web/lib/apiAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "https://money-circle-api.vercel.app",
   timeout: 20000,
   headers: {
     "Content-Type": "application/json",
